7fa6fe0f20165b597d18b018f8311558ea6db4f7
mtakamichi <takamichi.miyata@gmail.com>
2018-12-12 08:51:28 +0900

    Prob.11と12

---
+++ Python/Problem_012.py
@@ -0,0 +1,21 @@
+
+#!/usr/bin/env,python3
+#,-*-,coding:,utf-8,-*-
+'''
+Problem 12
+What is the value of the first triangle number to have over five hundred divisors?
+'''
+n=1
+while(True):
+    t=int(n*(n+1)/2)
+    sqt = int(t**0.5)
+    divl = [t%x==0 for x in range(2,sqt+1)]
+    numd = sum(divl)*2+2
+    # for the case of square triangle number
+    if(abs(sqt**2 -t) <10e-6):
+        numd-=1
+    if(numd>=10):
+        break
+    n+=1
+    print(t,sqt, numd)
+print(t)
