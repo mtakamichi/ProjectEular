564c08a3f947d7b68999165244e721ca9346e6b2
mtakamichi <takamichi.miyata@gmail.com>
2019-01-07 12:53:20 +0900

    Minor change

--- Python/Problem_028.py
+++ Python/Problem_028.py
@@ -4,5 +4,5 @@
 Problem 28
 What is the sum of the numbers on the diagonals in a 1001 by 1001 spiral formed in the same way?
 '''
-# 502 = (1001+1)/2+1
-print(1 + sum([16*x**2 - 28*x + 16 for x in range(2, 502)]))
+# 500 = (1001-1)/2
+print(1 + sum([16*x**2 + 4*x + 4 for x in range(1, 500+1)]))
