4553e49f776bcfae4e86e2bc6ea861a5ea898284
Takamichi Miyata <takamchi.miyata@gmail.com>
2019-01-16 08:18:43 +0900

    one line slution of Prob. 34

--- Python/Problem_034.py
+++ Python/Problem_034.py
@@ -8,9 +8,12 @@ from functools import reduce
 from operator import add
 from math import factorial
 
-ans = 0
+print(sum(filter(lambda x: x == reduce(add, map(factorial, list(map(int, list(str(x)))))), range(3, factorial(9)*7))))
+
+""" ans = 0
 for i in range(3, factorial(9)*7):
     nl = list(map(int, list(str(i))))
     if i == reduce(add, map(factorial, nl)):
         ans += i
 print(ans)
+ """
