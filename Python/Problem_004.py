d22167ba3072e7494a473ecea603163635770319
Takamichi Miyata <takamchi.miyata@gmail.com>
2019-01-24 10:36:20 +0900

    Lint some codes

--- Python/Problem_004.py
+++ Python/Problem_004.py
@@ -6,8 +6,8 @@ A palindromic number reads the same both ways. The largest palindrome made from 
 Find the largest palindrome made from the product of two 3-digit numbers.
 '''
 def is_div3(p): #pを3桁の数で割ったとき，割り切れてかつ商が3桁になるようなものが1つでもあるか？
-    return len(list(filter(lambda z: p/z < 1000, filter(lambda z: p%z==0, range(100,1000))))) !=0
+    return len(list(filter(lambda z: p/z < 1000, filter(lambda z: p%z == 0, range(100, 1000))))) != 0
 
 #6桁の回文数全体の集合P3
-P3=[100001*i + 10010*j + 1100*k for i in range(1,10) for j in range(10) for k in range(10)]
+P3=[100001*i + 10010*j + 1100*k for i in range(1, 10) for j in range(10) for k in range(10)]
 print(max(filter(is_div3, P3)))
--- Python/Problem_019.py
+++ Python/Problem_019.py
@@ -6,11 +6,11 @@ How many Sundays fell on the first of the month during the twentieth century?
 '''
 from itertools import accumulate, chain
 
-common_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-leap_days = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+common_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+leap_days = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
 def year2days(y):
-    if((y%4==0 and y%100!=0) or y%400==0 ):
+    if (y%4 == 0 and y%100 != 0) or y%400 == 0:
         return leap_days
     else:
         return common_days
