58814ea87da10739bfee6ebca0456298a375f4ae
Takamichi Miyata <takamchi.miyata@gmail.com>
2019-02-11 14:11:35 +0900

    Add one-line solution

--- Python/Problem_041.py
+++ Python/Problem_041.py
@@ -12,13 +12,15 @@ def is_prime(n):
         return False
     return all(n % i for i in range(3, int(math.sqrt(n)) + 1, 2))
 
+
 pp_list = []
-# Since the pandigital numbers made by n=5, 6, 8, 9 are all divisible by 3, 
+# Since all pandigital numbers made by n=5, 6, 8, 9 are divisible by 3, 
 # we only need to find the maximum prime number from the 7-pandigital numbers.
-pl = permutations(range(1, 8))
-for p in pl:
+for p in permutations(range(1, 8)):
     a = int(''.join(map(str, p)))
     if is_prime(a):
         pp_list.append(a)
-
 print(max(pp_list))
+
+# One-line solution
+#print(max([a for a in [int(''.join(map(str, p))) for p in permutations(range(1, 8))] if is_prime(a)]))
